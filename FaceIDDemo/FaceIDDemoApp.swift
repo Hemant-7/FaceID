b107aa21ca3151fa6da25eaa813340fd49259b02
Hemant-7 <hmntkmr285@gmail.com>
2024-06-08 19:23:23 +0530

    Initial Commit

---
+++ FaceIDDemo/FaceIDDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIDDemoApp.swift
+//  FaceIDDemo
+//
+//  Created by Hemant kumar on 08/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceIDDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
